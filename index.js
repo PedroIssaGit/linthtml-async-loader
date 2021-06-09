784a6761b73571b78fc1b5b1959743a296a4b4f1
pedroissa <pedrohenriqueissa@gmail.com>
2021-06-09 10:42:41 -0300

    Changing linter to @linthtml/linthtml

--- index.js
+++ index.js
@@ -1,110 +1,124 @@
-'use strict'
+"use strict";
 
-const { isString } = require('lodash')
-const { defaultTo } = require('lodash')
-const { escapeRegExp } = require('lodash')
+const { isString } = require("lodash");
+const { defaultTo } = require("lodash");
+const { escapeRegExp } = require("lodash");
 
-const path = require('path')
-const chalk = require('chalk')
-const Table = require('text-table')
-const htmllint = require('htmllint')
-const stripAnsi = require('strip-ansi')
+const path = require("path");
+const chalk = require("chalk");
+const Table = require("text-table");
+const linthtml = require("@linthtml/linthtml");
+const stripAnsi = require("strip-ansi");
 
-const { getOptions } = require('loader-utils')
-const { cosmiconfig } = require('cosmiconfig')
+const { getOptions } = require("loader-utils");
+const { cosmiconfig } = require("cosmiconfig");
 
-const explorer = cosmiconfig('htmllint')
+const explorer = cosmiconfig("linthtml");
 
-module.exports = htmllintLoader
+module.exports = linthtmlLoader;
 
-function HtmlLintError(message) {
-  Error.call(this)
-  this.name = 'HtmlLintError'
-  this.message = message
-  Error.captureStackTrace(this, HtmlLintError)
+function linthtmlError(message) {
+  Error.call(this);
+  this.name = "linthtmlError";
+  this.message = message;
+  Error.captureStackTrace(this, linthtmlError);
 }
 
-HtmlLintError.prototype = Object.create(Error.prototype)
-HtmlLintError.prototype.constructor = HtmlLintError
+linthtmlError.prototype = Object.create(Error.prototype);
+linthtmlError.prototype.constructor = linthtmlError;
 
 function pluralize(word, count) {
-  return (count === 1 ? word : `${word}s`)
+  return count === 1 ? word : `${word}s`;
 }
 
 function stringLength(str) {
-  return stripAnsi(str).length
+  return stripAnsi(str).length;
 }
 
 function renderIssue(issue) {
-  if (issue.code === 'E011' && issue.data.value && issue.data.format) {
-    return `'${issue.data.value}' must match ${issue.data.format}`
+  if (issue.code === "E011" && issue.data.value && issue.data.format) {
+    return `'${issue.data.value}' must match ${issue.data.format}`;
   } else {
-    return htmllint.messages.renderIssue(issue)
+    return linthtml.messages.renderIssue(issue);
   }
 }
 
 function stylish(resourcePath, issues) {
-  const align = [ '', 'r', 'c', 'l' ]
-  const problem = 'problem'
-  const separator = ':line-col-separator:'
-  const separatorRE = RegExp(`\\s*${escapeRegExp(separator)}\\s*`, 'g')
-  const separatorReplacer = chalk.white.dim(':')
+  const align = ["", "r", "c", "l"];
+  const problem = "problem";
+  const separator = ":line-col-separator:";
+  const separatorRE = RegExp(`\\s*${escapeRegExp(separator)}\\s*`, "g");
+  const separatorReplacer = chalk.white.dim(":");
 
-  const rows = issues.map((issue) => ([
-    '',
+  const rows = issues.map((issue) => [
+    "",
     chalk.white.dim(`${issue.line}`),
     separator,
     chalk.white.dim(`${issue.line}`),
     chalk.yellow(problem),
     chalk.white(renderIssue(issue)),
-    chalk.white.dim(issue.rule)
-  ]))
+    chalk.white.dim(issue.rule),
+  ]);
 
-  const header = chalk.white.underline(resourcePath)
-  const table = Table(rows, { align, stringLength }).replace(separatorRE, separatorReplacer)
-  const footer = chalk.yellow.bold(`\u2716 HtmlLint found ${issues.length} ${pluralize('problem')}`)
-  const output = `\n${header}\n${table}\n\n${footer}\n`
+  const header = chalk.white.underline(resourcePath);
+  const table = Table(rows, { align, stringLength }).replace(
+    separatorRE,
+    separatorReplacer
+  );
+  const footer = chalk.yellow.bold(
+    `\u2716 linthtml found ${issues.length} ${pluralize("problem")}`
+  );
+  const output = `\n${header}\n${table}\n\n${footer}\n`;
 
-  return output
+  return output;
 }
 
 function findConfig(configPath, resourcePath) {
   return isString(configPath)
     ? explorer.load(configPath)
-    : explorer.search(path.dirname(resourcePath))
+    : explorer.search(path.dirname(resourcePath));
 }
 
-function htmllintLoader(source) {
-  const webpack = this
-  const options = defaultTo(getOptions(webpack), {})
-  const callback = webpack.async()
-  webpack.cacheable()
+function linthtmlLoader(source) {
+  const webpack = this;
+  const options = defaultTo(getOptions(webpack), {});
+  const callback = webpack.async();
+  webpack.cacheable();
 
-  const cwd = process.cwd()
-  const shortResourcePath = webpack.resourcePath.indexOf(cwd) === 0
-    ? webpack.resourcePath.substr(cwd.length + 1)
-    : webpack.resourcePath
+  const cwd = process.cwd();
+  const shortResourcePath =
+    webpack.resourcePath.indexOf(cwd) === 0
+      ? webpack.resourcePath.substr(cwd.length + 1)
+      : webpack.resourcePath;
 
-  findConfig(options.config, webpack.resourcePath).then((result) => {
-    if (!result || !result.config) {
-      throw new HtmlLintError(`cannot find config for ${webpack.resourcePath}!`)
-    }
+  findConfig(options.config, webpack.resourcePath)
+    .then((result) => {
+      if (!result || !result.config) {
+        throw new linthtmlError(
+          `cannot find config for ${webpack.resourcePath}!`
+        );
+      }
 
-    webpack.addDependency(result.filepath)
-    htmllint.use(result.config.plugins || [])
-    delete result.config.plugins
-    return htmllint(source, result.config)
-  }).then((issues) => {
-    if (issues.length !== 0) {
-      const output = stylish(shortResourcePath, issues)
-      const report = new HtmlLintError(output)
-      options.failOnProblem
-        ? webpack.emitError(report)
-        : webpack.emitWarning(report)
-    }
-  }).then(() => {
-    callback(null, source)
-  }, (err) => {
-    callback(err)
-  })
+      webpack.addDependency(result.filepath);
+      linthtml.use(result.config.plugins || []);
+      delete result.config.plugins;
+      return linthtml(source, result.config);
+    })
+    .then((issues) => {
+      if (issues.length !== 0) {
+        const output = stylish(shortResourcePath, issues);
+        const report = new linthtmlError(output);
+        options.failOnProblem
+          ? webpack.emitError(report)
+          : webpack.emitWarning(report);
+      }
+    })
+    .then(
+      () => {
+        callback(null, source);
+      },
+      (err) => {
+        callback(err);
+      }
+    );
 }
